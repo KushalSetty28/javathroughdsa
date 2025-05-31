6a05a099f6944042304b3e5caea8dc3c93ac31ec
Sai Kushal Bachu <bachukushal28@gmail.com>
2025-05-31 12:22:38 +0530

    Add files via upload

---
+++ OverloadingExample.java
@@ -0,0 +1,24 @@
+package dsa;
+class Calculator{
+	int cal(int a, int b) {
+		return a+b;
+	}
+	double cal(double a, double b) {
+		return a*b;
+	}
+	int cal(int a, int b, int c) {
+		return a*b*c;
+	}
+}
+public class OverloadingExample {
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		System.out.println(c.cal(3,6));
+		System.out.println(c.cal(3.5, 4.9));
+		System.out.println(c.cal(2,3,5));
+		
+	}
+
+}
+ // method overloading means having multiple methods with the same name for different parameters with same class
